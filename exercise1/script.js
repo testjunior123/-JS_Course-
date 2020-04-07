b5b98753a4e7432e2cac2b25a7bccb03fb74a86e
testjunior123 <rynekdruku3d@gmail.com>
2020-04-07 14:30:05 +0200

    [the next update of exercise1]

--- exercise1/script.js
+++ exercise1/script.js
@@ -1,1 +1,51 @@
-console.log("Hi boddy");
+// variable and data type
+var FirstName= 'John';
+var age= 30;
+console.log(FirstName + ' ' + age);
+
+// typoe coecion
+var job, isMarried;
+job ='teacher';
+isMarried = true;
+
+console.log(FirstName + ' is married? '  + isMarried );
+
+//variable mutation
+age = 'thirty'; //the variable is already defined so we don't need to put "var"
+
+alert(FirstName + ' ' + age);
+
+var lastName = prompt('What is his last name?');
+console.log(FirstName + ' ' + lastName);
+
+// Basic operators
+//typeof operator
+
+var z;
+console.log(typeof isMarried + ' ' + typeof z);
+
+//operator precedense
+
+var now = 2020;
+var damianAge = prompt('What is Damian age');
+var fullAge = 18;
+
+var damianIsFullAge = damianAge >= fullAge;
+
+console.log(damianIsFullAge);
+
+alert('Damian is full age? ' + damianIsFullAge);
+
+//grouping
+var x,y;
+
+x=y=(3+3)*2-1;
+console.log(x,y);
+
+x *=2; //the same as x=x*2
+
+console.log(x);
+
+x++ // as x+2
+
+console.log(x);
